f384d48a8c45da8ae1ab71090f6839321b8607ad
azaheer28 <azaheer28@students.gilman.edu>
2026-07-03 12:20:53 -0400

    first working comit!!! using paddle ocr, workign perfectly!!!

--- NASA_Components_Inventory_Code.py
+++ NASA_Components_Inventory_Code.py
@@ -1,33 +1,18 @@
 import numpy as np
 import cv2
-import ollama
 import os
-import easyocr
-'''def question ():
-    prompt = """you are a professional image analyzer. you are the world's best analyzer, so I chose you. 
-    I want you to look at this image and analyze it carefully. 
-    I want you to extract the part number the description, the size, the date code, the Lot code, and the quantity.
-     It is very important that you make sense of these numbers, so that they arent random but actually logical and therefore correct.
-      you may not see the size of the component in the label, but you can just look at the part number and extract adn translate the size from there.
-       if a value on the lable is crossed out, I want you to look at the handwritten note on there that corrects it.
-        if you still can't find that number that corrects the crossed out number, and this goes for any value, if its crossed out, then look for the correction,
-         then if you still cnat find it, output '?'. DO NOT USE COMMAS in the output, and not spaces, just use underscores_ instead.
-         After carefully analyzing the label, ensuring high accuracy, then I want you to return to me with the following return syntax: 
-         partnumber$Description$Size$Date_code$Lot_Code$quantity$"""
+from paddleocr import PaddleOCR
 
-
-    print("sending to ollama...")
-    responce = ollama.chat (
-        model = 'llava',
-        messages = [{
-            "role":"user", 
-            "content":prompt,
-            'images': [open('label.jpg', 'rb').read()]
-            }]
-    )
-    return responce['message']['content']'''
 
 cap = cv2.VideoCapture(0)
+
+#-----------------------------------------------------------OCR initialization
+ocr = PaddleOCR(
+    lang='en', 
+    use_textline_orientation=False, 
+    use_doc_orientation_classify=False,
+    enable_mkldnn=False
+)
 
 while True:
     ret, frame = cap.read()
@@ -36,7 +21,7 @@ while True:
     #to black and white
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     #denoising
-    
+    #cv2.fastNlMeansDenoising(frame)
 
     key = cv2.waitKey(1)
 
@@ -48,12 +33,11 @@ while True:
         cv2.imshow('captured snapshot',frame)
         
         if os.path.exists("label.jpg"):
-
-            reader = easyocr.Reader(['en'])
-            result = reader.readtext("label.jpg", detail = 0)
+#------------------------------------------------------------------------------------------OCR Reading            
+            result = ocr.predict("label.jpg")
+            result = result[0]['rec_texts']
 
             print(result)
-
         else:
             print("NO IMAGE TAKEN BUDDY!")
 
@@ -62,7 +46,7 @@ cv2.destroyAllWindows()
 
 
 
-#-----------------------------------------------------------------------------------------------------------------------------------PANDAS experimental testing
+#---------------------------------------------------------------------------------------------------------------PANDAS experimental testing
 '''responce = "M55342K11B10E0R$RESISTOR_10k_OHMS_1%$0402$0406J$124168$570"
 Box = str("1")
 
@@ -74,10 +58,5 @@ print(row)
 #print(row[6])
 
 
-d;fkanwd/flkanwf/lnkw/lgjrwaebgqlejbnekj;rk
+
 '''
-
-
-
-
-

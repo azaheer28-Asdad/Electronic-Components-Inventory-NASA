ded96e92410bd90f9ac5a03e6cf9b0858baa1c3b
azaheer28 <azaheer28@students.gilman.edu>
2026-07-04 20:10:44 -0400

    working sorted list, missign size val

--- NASA_Components_Inventory_Code.py
+++ NASA_Components_Inventory_Code.py
@@ -2,12 +2,10 @@ import numpy as np
 import cv2
 import os
 from paddleocr import PaddleOCR
-import re
 
 
 
-
-def label_box(result_text, match, result):
+def label_val(result_text, match, result):
 
     for index, item in enumerate(result_text):
         if item == match:
@@ -33,7 +31,44 @@ def label_box(result_text, match, result):
     x3 = anchor_box[3][0]
     y3 = anchor_box[3][1]
 
-    return x0, y0, x1, y1, x2, y2, x3, y3
+    #---------------------------------------------------------------calculating val
+    #-----------------------------------------------------------------------------------calc slope
+    # Top line runs from Point 0 to Point 1
+    m_top = (y1 - y0) / ((x1 - x0) + 1e-5)
+
+    # Bottom line runs from Point 3 to Point 2
+    m_bottom = (y2 - y3) / ((x2 - x3) + 1e-5)
+
+    best_match_text = None
+    shortest_distance = float('inf') # Start with an infinitely large distance
+
+    for cand_index, cand_text in enumerate(result_text):
+        if cand_text == match:
+            continue
+
+        # Grab the bounding box for this specific candidate word
+        cand_box = result[0]['dt_polys'][cand_index]
+        
+        # Extract the Top-Left corner of this candidate
+        cx0, cy0 = cand_box[0][0], cand_box[0][1]
+        
+        # Extract the Bottom-Left corner
+        cx3, cy3 = cand_box[3][0], cand_box[3][1]
+
+        if cx0 > x1:
+            expected_y_top = (m_top * (cx0 - x1)) + y1
+            expected_y_bottom = (m_bottom * (cx0 - x2)) + y2
+
+            buffer = 0
+
+            if (expected_y_top <= (cy3 + buffer)) and (expected_y_bottom >= (cy0 - buffer)):
+                
+                dist = cx0 - x1
+                if dist < shortest_distance:
+                    shortest_distance = dist
+                    best_match_text = cand_text
+
+    return best_match_text
 
 
 
@@ -65,7 +100,7 @@ while True:
     if key == ord(' '):
         cv2.imwrite("label.jpg", frame)
         cv2.imshow('captured snapshot',frame)
-        
+       
         if os.path.exists("label.jpg"):
 #------------------------------------------------------------------------------------------OCR Reading            
             result = ocr.predict("label.jpg")
@@ -74,67 +109,44 @@ while True:
             print(result_text)
         else:
             print("NO IMAGE TAKEN BUDDY!")
+#---------------------------------------------------------------------------------------------------------run all processing code here (2 tab):
+#------------------------------------------------------------------------------------call labeling func
+        #label (all caps, pay attension!!) MAKE SURE THE VALS DONT REPEAT!!!!!!:
+
+        box_number = 1 #insert box number!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        sorted_list = []
+
+        #part number:
+        match = 'OUR'
+        best_match_text = label_val(result_text, match, result)
+        sorted_list.insert(0,best_match_text)
+        #description:
+        match = 'VALUE'
+        best_match_text = label_val(result_text, match, result)
+        sorted_list.insert(1,best_match_text)
+        #size!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:
+        #date Code:
+        match = 'CODE'
+        best_match_text = label_val(result_text, match, result)
+        sorted_list.insert(3,best_match_text)
+        #Lot Code:
+        match = 'LOT'
+        best_match_text = label_val(result_text, match, result)
+        sorted_list.insert(4,best_match_text)
+        #quantity:
+        match = 'QTY.'
+        best_match_text = label_val(result_text, match, result)
+        sorted_list.insert(5,best_match_text)
+        #box number!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!:
+        sorted_list.insert(6,box_number)
+
+        print(sorted_list)
+#--------------------------------------------------------------------call labeling func        
+
+
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-#-----------------------------------------------------------------------------------call function for label rect vals
-match = 'OUR'
-
-x0, y0, x1, y1, x2, y2, x3, y3 = label_box(result_text, match, result)
-
-#-----------------------------------------------------------------------------------calc slope
-# Top line runs from Point 0 to Point 1
-m_top = (y1 - y0) / ((x1 - x0) + 1e-5)
-
-# Bottom line runs from Point 3 to Point 2
-m_bottom = (y2 - y3) / ((x2 - x3) + 1e-5)
-
-best_match_text = None
-shortest_distance = float('inf') # Start with an infinitely large distance
-
-for cand_index, cand_text in enumerate(result_text):
-    if cand_text == match:
-        continue
-
-    # Grab the bounding box for this specific candidate word
-    cand_box = result[0]['dt_polys'][cand_index]
-    
-    # Extract the Top-Left corner of this candidate
-    cx0, cy0 = cand_box[0][0], cand_box[0][1]
-    
-    # Extract the Bottom-Left corner
-    cx3, cy3 = cand_box[3][0], cand_box[3][1]
-
-    if cx0 > x1:
-        expected_y_top = (m_top * (cx0 - x1)) + y1
-        expected_y_bottom = (m_bottom * (cx0 - x2)) + y2
-
-        buffer = 0
-
-        if (expected_y_top <= (cy3 + buffer)) and (expected_y_bottom >= (cy0 - buffer)):
-            
-            dist = cx0 - x1
-            if dist < shortest_distance:
-                shortest_distance = dist
-                best_match_text = cand_text
-
-
-if best_match_text:
-    print(f"BINGO! The value closest to 'OUR' is: {best_match_text}")
-else:
-    print("Could not find any overlapping values to the right.")
-
-#make function out of all of that! and then find a way to use it without pressing q and just right after the space, maybe call it right after space is pressed or smth
-#change this for all of the vals ou want by doing the above and changing that one word. also dont forget to add the stuff in the () for the def func
-
-
-
-
-
-
-
 
 
 
@@ -149,6 +161,7 @@ row.append(Box)
 
 print(row)
 #print(row[6])
+
 
 
 
